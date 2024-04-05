ed212496dac79ceae34a0290379f969ae20cea48
Sameer Katija <sameerkatija@gmail.com>
2024-04-05 21:22:31 +0500

    intro-to-js

---
+++ main.js
@@ -0,0 +1,21 @@
+// // let name = "Sameer";
+// // alert(name + " katija"');
+// // var sch = "abab";
+
+// // alert(sch);
+
+// // alert(3 % 2);
+
+// let number = Number(prompt("Enter a num: "));
+
+// // if (number % 2 !== 0) {
+// //   alert("Odd");
+// // } else {
+// //   alert("Even");
+// // }
+
+let num = 1;
+while (num < 1000) {
+  console.log(num);
+  num = num + 2;
+}
